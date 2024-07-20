f1b14f49eecc068a490d48b24af5cfba7be970ca
Allanmbt <130887114+Allanmbt@users.noreply.github.com>
2024-07-21 00:00:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,32 +11,24 @@ import os
 import traceback
 
 app = Flask(__name__)
-static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
 
-# Channel Access Token
+# Channel Access Token and Secret from environment variables
 line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
-# Channel Secret
 handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 
-# 监控所有来自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
-    # 获取 X-Line-Signature header 值
     signature = request.headers['X-Line-Signature']
-    # 获取请求 body 的文本
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-    # 处理 webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
     return 'OK'
 
-# 处理消息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    msg = event.message.text
     try:
         line_bot_api.reply_message(event.reply_token, TextSendMessage('欢迎加入cbody'))
     except:
@@ -44,7 +36,7 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, TextSendMessage('发生错误，请稍后再试。'))
 
 @handler.add(PostbackEvent)
-def handle_message(event):
+def handle_postback(event):
     print(event.postback.data)
 
 @handler.add(MemberJoinedEvent)
